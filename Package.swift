f3e9524d5e4e9d247cee76fea48c2745b466c382
Martin Kim Dung-Pham <kim@elbedev.com>
2017-10-15 18:22:57 +0200

    Expose Commands as part of the package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Commands",
+    products: [
+        .library(name: "Commands", targets: ["Commands"]),
+    ],
     dependencies: [],
     targets: [
         .target(
