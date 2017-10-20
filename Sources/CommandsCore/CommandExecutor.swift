509c8f3a75f2f4b8c4b73b39ba8d19f7928564f5
Martin Kim Dung-Pham <kim@elbedev.com>
2017-10-20 22:04:16 +0200

    wip

--- Sources/CommandsCore/CommandExecutor.swift
+++ Sources/CommandsCore/CommandExecutor.swift
@@ -13,7 +13,8 @@ public class CommandExecutor {
     let arguments: [String]
     let outputStream: OutputStream
 
-    public init(launchPath: String, arguments: [String], outputStream: OutputStream = StandardOutOutputStream()) {
+    public init(launchPath: String, arguments: [String],
+                outputStream: OutputStream = StandardOutOutputStream()) {
         self.launchPath = launchPath
         self.arguments = arguments
         self.outputStream = outputStream
@@ -23,13 +24,22 @@ public class CommandExecutor {
         let process = Process()
         process.launchPath = launchPath
         process.arguments = arguments
-        let pipe = Pipe()
-        process.standardOutput = pipe
-        pipe.fileHandleForReading.readabilityHandler = { [weak self] handle in
+
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+        outputPipe.fileHandleForReading.readabilityHandler = { [weak self] handle in
             let data = handle.availableData
             self?.outputStream.write([UInt8](data), maxLength: data.count)
         }
+
+        let inputPipe = Pipe()
+        process.standardInput = inputPipe
         process.launch()
+
+        while let input = readLine() {
+            inputPipe.fileHandleForWriting.write("\(input)\n".data(using: .utf8)!)
+        }
+
         process.waitUntilExit()
     }
 }
