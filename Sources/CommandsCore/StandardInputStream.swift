1005a85858a7e50e3787a283859d824a9adf4bc7
Martin Kim Dung-Pham <kim@elbedev.com>
2017-10-20 22:03:06 +0200

    Add StandardInputStream

---
+++ Sources/CommandsCore/StandardInputStream.swift
@@ -0,0 +1,12 @@
+//
+//  StandardInputStream.swift
+//  CommandsPackageDescription
+//
+//  Created by Martin Kim Dung-Pham on 20.10.17.
+//
+
+import Foundation
+
+public class StandardInputStream: InputStream {
+
+}
