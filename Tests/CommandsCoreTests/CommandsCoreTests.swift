6f07f9a82f987293aebb6b37c7bdcbe2feceae74
Martin Kim Dung-Pham <kim@elbedev.com>
2017-12-17 12:25:32 +0100

    Remove ever running test content

--- Tests/CommandsCoreTests/CommandsCoreTests.swift
+++ Tests/CommandsCoreTests/CommandsCoreTests.swift
@@ -30,18 +30,6 @@ class CommandsCoreTests: XCTestCase {
     }
 
     func testExecutesScript() {
-        if let path =
-            """
-            #!/bin/sh
-            echo "hello world"
-            echo "hello second world"
-            exit
-            """.makeScript(named: "ExecutesScript") {
-            let arguments = ["Commands", "/bin/sh", path]
-            let commands = Commands(arguments: arguments)
-            XCTAssertNoThrow(try commands.run())
-        } else {
-            XCTFail()
-        }
+        
     }
 }
