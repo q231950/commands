4d84957f552d6253d652d25df7d0ea2e21f271d1
Martin Kim Dung-Pham <kim@elbedev.com>
2018-01-14 12:01:41 +0100

    Make Command init public

--- Sources/CommandsCore/Command.swift
+++ Sources/CommandsCore/Command.swift
@@ -2,7 +2,7 @@ public struct Command {
     let launchPath: String
     let arguments: [String]
 
-    init(launchPath: String, arguments: [String] = []) {
+    public init(launchPath: String, arguments: [String] = []) {
         self.launchPath = launchPath
         self.arguments = arguments
     }
