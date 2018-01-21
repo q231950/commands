ca7af78a62a5e41f25dadca9ac1c45984b2dd134
Martin Kim Dung-Pham <kim@elbedev.com>
2018-01-21 18:43:34 +0100

    🐮

--- test-target-resolver.swift
+++ test-target-resolver.swift
@@ -7,7 +7,7 @@ import Foundation
 // @see https://developer.apple.com/library/content/technotes/tn2339/_index.html#//apple_ref/doc/uid/DTS40014588-CH1-PRODUCT
 func resolve(filePaths: String) -> String {
     //let paths = filePaths.split(separator: "\n")
-    return "CommandsTests"
+    return "-only-testing:CommandsTests -only-testing:CommandsCoreTests"
 }
 
 let args = CommandLine.arguments
