1e7711523978b267e9a349fd9a0ca35bab5be14d
Martin Kim Dung-Pham <kim@elbedev.com>
2018-01-21 18:31:41 +0100

    Groovy debugging

--- test-target-resolver.swift
+++ test-target-resolver.swift
@@ -7,7 +7,6 @@ import Foundation
 // @see https://developer.apple.com/library/content/technotes/tn2339/_index.html#//apple_ref/doc/uid/DTS40014588-CH1-PRODUCT
 func resolve(filePaths: String) -> String {
     let paths = filePaths.split(separator: "\n")
-    print("\(paths)")
     return "CommandsTests"
 }
 
@@ -22,3 +21,5 @@ if args.count > 1 {
     print("Nothing changed")
     exit(1)
 }
+
+print("should never reach here")
